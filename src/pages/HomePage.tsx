89796c77465342a29581c27a205168fa7afc5e62
JCWoodworker <jc@rilocalwoodworks.com>
2024-02-29 15:35:51 -0500

    set up logic to enter board id and go to it's home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,44 @@
+import { useState } from "react"
 import { Typography } from "@mui/material"
+import { useNavigate } from "react-router-dom"
 
 const HomePage: React.FC = () => {
+	const [myBoardId, setMyBoardId] = useState<number>(0)
+	const navigate = useNavigate()
+
+	const handleSetBoardId = (boardId: number) => {
+		setMyBoardId(boardId)
+	}
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const boardId = Number(e.target.value)
+		handleSetBoardId(boardId)
+	}
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
+		handleSetBoardId(myBoardId)
+		navigate(`/boards/${myBoardId}`)
+	}
+
+	console.log(myBoardId)
+
 	return (
 		<>
-			<Typography variant="h1">My Cutting Board</Typography>
-			<form>
+			<Typography variant="h2">My Cutting Board</Typography>
+			<br />
+			<form className="form" onSubmit={handleSubmit}>
 				<label>Enter Your Board ID</label>
-				<input type="text" />
-				<button type="submit">Submit</button>
+				<input
+					className="form-input-box"
+					type="float"
+					name="boardId"
+					onChange={handleChange}
+					value={myBoardId}
+				/>
+				<button className="form-submit" type="submit">
+					Submit
+				</button>
 			</form>
 		</>
 	)
