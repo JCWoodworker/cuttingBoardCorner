4429e893039f2da5ff5c190b1cf4fcd94e169004
JCWoodworker <jc@rilocalwoodworks.com>
2024-02-29 06:46:30 -0500

    added mui and roboto font

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { useState } from "react"
 import "./App.css"
+import HomePage from "./pages/HomePage"
 
 function App() {
-	const [count, setCount] = useState(0)
-
-	return (
-		<>
-			<div></div>
-		</>
-	)
+	return <HomePage />
 }
 
 export default App
