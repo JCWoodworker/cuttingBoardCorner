75278c7077a3fc95bf85dba633201ec7ab7638ec
JCWoodworker <jc@rilocalwoodworks.com>
2024-03-02 10:59:26 -0500

    forgot to add hyperlink to contact info

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,7 +3,7 @@ import { Link } from "@mui/material"
 const Contact = () => {
 	return (
 		<div>
-			<Link>RI Local Woodworks</Link>
+			<Link href="https://www.rilocalwoodworks.com/">RI Local Woodworks</Link>
 		</div>
 	)
 }
