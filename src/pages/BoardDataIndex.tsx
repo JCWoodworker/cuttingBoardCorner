2a6bfd9585f02fc576f3a1c6519b0510d845b266
JCWoodworker <jc@rilocalwoodworks.com>
2024-03-04 16:13:10 -0500

    memoized call to backend so refresh doesn't cause multiple calls

--- src/pages/BoardDataIndex.tsx
+++ src/pages/BoardDataIndex.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useMemo } from "react"
 import { useParams } from "react-router-dom"
 
 import axios from "axios"
@@ -14,24 +14,24 @@ const BoardDataIndex = () => {
 	const [boardData, setBoardData] = useState({})
 	const [error, setError] = useState<boolean>(false)
 
-	const fetchBoardData = async () => {
-		try {
-			const urlPrefix = await getBackendUrl()
-			const response = await axios.get(
-				`${urlPrefix}/subapps/mycuttingboard/boards/${boardId}`
-			)
-			if (response.status === 200) {
-				const data = await response.data
-				setBoardData(data)
-			} else {
+	useMemo(() => {
+		const fetchBoardData = async () => {
+			try {
+				const urlPrefix = await getBackendUrl()
+				const response = await axios.get(
+					`${urlPrefix}/subapps/mycuttingboard/boards/${boardId}`
+				)
+				if (response.status === 200) {
+					const data = await response.data
+					setBoardData(data)
+				} else {
+					setError(true)
+				}
+			} catch (error) {
 				setError(true)
 			}
-		} catch (error) {
-			setError(true)
 		}
-	}
 
-	useEffect(() => {
 		fetchBoardData()
 	}, [boardId])
 
