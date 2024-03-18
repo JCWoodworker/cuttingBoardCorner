33cf021db9f28963524a18195aabec32e89c8cb3
JCWoodworker <jc@rilocalwoodworks.com>
2024-03-18 17:56:30 -0400

    added loading message

--- src/pages/BoardDataShow.tsx
+++ src/pages/BoardDataShow.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Box, Typography } from "@mui/material"
 import NotFound from "./NotFound"
 import CaringForYourBoard from "./CaringForYourBoard"
@@ -9,6 +10,8 @@ interface Props {
 }
 
 const BoardDataShow: React.FC<Props> = ({ boardData }) => {
+	const [isLoading, setIsLoading] = useState(true)
+
 	if (!boardData) {
 		return <NotFound />
 	}
@@ -21,10 +24,19 @@ const BoardDataShow: React.FC<Props> = ({ boardData }) => {
 					{boardData.board_description}
 				</Typography>
 				<Typography variant="subtitle2">ID: {boardData.id}</Typography>
-				<img
-					src={boardData.board_image_url}
-					className="cutting-board-image"
-				></img>
+				<Box sx={{ my: 2, textAlign: "center", height: "auto" }}>
+					{isLoading && (
+						<Box sx={{ mt: 2, textAlign: "center" }}>
+							<span>Loading Image ...</span>
+						</Box>
+					)}
+					<img
+						src={boardData.board_image_url}
+						className="cutting-board-image"
+						onLoad={() => setIsLoading(false)}
+						style={{ display: isLoading ? "none" : "block" }}
+					></img>
+				</Box>
 				<Typography variant="h5" sx={{ fontWeight: "bold" }}>
 					{boardData.customer_message}
 				</Typography>
