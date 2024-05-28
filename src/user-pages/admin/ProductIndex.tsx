76c3aebcc9195d2bdedae9ed2824df0700397462
JCWoodworker <jc@rilocalwoodworks.com>
2024-05-28 00:55:38 -0400

    added test get for new admin endpoint

--- src/user-pages/admin/ProductIndex.tsx
+++ src/user-pages/admin/ProductIndex.tsx
@@ -1,13 +1,37 @@
+import { useState, useEffect } from "react"
 import { Box, Typography } from "@mui/material"
+import { Requests } from "../../requests/Requests"
 
 import NavDrawer from "../../navigation/NavDrawer"
 
 const ProductIndex = () => {
+	const [testMessage, setTestMessage] = useState<string>("")
+
+	const getMessage = async () => {
+		const accessToken = localStorage.getItem("accessToken")
+		const response = await Requests.GET(
+			"/subapps/mycuttingboard/admin/test-message",
+			false,
+			true,
+			accessToken as string
+		)
+		setTestMessage(response.data.message)
+	}
+	
+	useEffect(() => {
+		getMessage()
+	}, [])
+
 	return (
 		<>
 			<NavDrawer />
 			<Box sx={{ pt: "3rem" }}>
 				<Typography variant="h4">All Products</Typography>
+				{testMessage ? (
+					<Typography variant="body1">{testMessage}</Typography>
+				) : (
+					<Typography variant="body1">No Test Message</Typography>
+				)}
 			</Box>
 		</>
 	)
