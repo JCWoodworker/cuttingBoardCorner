eba763961785a90fe0bc8987661728c68a0bd28c
JCWoodworker <jc@rilocalwoodworks.com>
2024-06-24 16:25:15 -0400

    finished user role switching functionality

--- src/user-pages/admin/user-components/UserRoleSwapButton.tsx
+++ src/user-pages/admin/user-components/UserRoleSwapButton.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Button } from "@mui/material"
 import useEnv from "../../../hooks/use-env"
 import useUserDataContext from "../../../hooks/use-user-data-context"
@@ -6,30 +7,34 @@ import { LocalStorageElements } from "../../../utils/clearLocalStorage"
 
 const UserRoleSwapButton = () => {
 	const { userInfo } = useUserDataContext()
+	const [userRole, setUserRole] = useState(userInfo?.role)
 	const environment = useEnv()
 	const accessToken = localStorage.getItem(LocalStorageElements.ACCESS_TOKEN)
 
 	if (!userInfo) return null
-  if (!accessToken) return null
+	if (!accessToken) return null
 	if (environment !== "dev" && environment !== "preprod") return null
-	const userRole = userInfo.role
 	const nextUserRole = userRole === "admin" ? "basic" : "admin"
 
 	const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
 		e.preventDefault()
-		const result = Requests.POST(
-			"/user/role",
-			{ role: nextUserRole },
-			true,
-			accessToken
-		)
-    console.log(result)
+		try {
+			await Requests.POST(
+				"/subapps/mycuttingboard/user-role-swap",
+				{ role: nextUserRole },
+				true,
+				accessToken
+			)
+			setUserRole(nextUserRole)
+		} catch (err) {
+			console.log(err)
+		}
 	}
 
 	return (
 		<>
 			<Button
-				onClick={() => handleClick}
+				onClick={handleClick}
 			>{`Switch user role to: ${nextUserRole}`}</Button>
 		</>
 	)
