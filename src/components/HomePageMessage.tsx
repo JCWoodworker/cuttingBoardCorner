3e9dfded7c0a67ff34eb7debc78681460a4b434e
JCWoodworker <jc@rilocalwoodworks.com>
2024-07-16 18:24:29 -0400

    styling updates and messaging updates on home page

--- src/components/HomePageMessage.tsx
+++ src/components/HomePageMessage.tsx
@@ -1,8 +1,8 @@
 import { Box, Typography } from "@mui/material"
 
 const homePageMessageList = [
-	`You will eventually be able to register any product(s) you've purchased or received while signed in to your account.`,
-	`For now, please sign in using the Google link below and I will manually update your account to display your product(s).`,
+	`Please sign up/in using the Google link below, then use the "Contact Me Directly" link to let me know who you are and what product(s) you own.  I will manually link them to your account.`,
+	`Once you've signed in you'll be able to view information about your product(s) and save links to your favorite recipes.  You can even add your own notes to each link`,
 	`If you were sent to this page after scanning an NFC chip in your board or coaster, it means that link is unfortunately broken.  Please contact me via the button below so I can fix it`,
 ]
 
@@ -15,7 +15,7 @@ const HomePageMessage = () => {
 			}}
 		>
 			<Box>
-				<Typography variant="h4">
+				<Typography variant="h6">
 					Thanks for your patience while I work on improving this site.
 				</Typography>
 			</Box>
@@ -29,17 +29,15 @@ const HomePageMessage = () => {
 			>
 				{homePageMessageList.map((message) => (
 					<Typography
+						key={message}
 						variant="body1"
 						sx={{
 							padding: "1rem",
 							fontSize: "1.1rem",
-							border: "3px solid rgba(33, 125, 255, 0.87)",
 							borderRadius: "0.5rem",
 							backgroundColor: "rgba(33, 125, 255, 0.1)",
 							minWidth: { xs: "100%", md: "70%" },
-							//last child
 							"&:last-child": {
-								border: "3px solid rgba(255, 0, 0, 0.87)",
 								backgroundColor: "rgba(255, 0, 0, 0.1)",
 							},
 						}}
